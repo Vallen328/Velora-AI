c0f540c2254b6e289db038cd7cd0ca9aa60a8e99
Vallen Dsouza <vallendsouza@gmail.com>
2026-05-26 00:55:52 +0530

    09: widget layout

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,22 +1,17 @@
-"use client";
+import { WidgetView } from "@/modules/widget/ui/views/widget-view";
 
-import { useVapi } from "@/modules/widget/hooks/use-vapi";
-import { Button } from "@workspace/ui/components/button";
+interface Props {
+  searchParams: {
+    organizationId?: string;
+  };
+}
 
-export default function Page() {
-  const {isSpeaking, isConnecting, isConnected, transcript, startCall, endCall,} = useVapi();
+const Page = ({ searchParams }: Props) => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh max-w-md mx-auto w-full">
-      <Button onClick={() => startCall()}>
-        Start call
-      </Button>
-      <Button onClick={() => endCall()} variant="destructive">
-        End call
-      </Button>
-      <p>IsConnected: {`${isConnected}`}</p>
-      <p>IsConnecting: {`${isConnecting}`}</p>
-      <p>IsSpeaking: {`${isSpeaking}`}</p>
-      <p>{JSON.stringify(transcript, null, 2)}</p>
-    </div>
-  )
-} 
+    <WidgetView
+      organizationId={searchParams.organizationId || ""}
+    />
+  );
+};
+
+export default Page;
