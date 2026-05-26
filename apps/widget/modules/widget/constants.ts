2330e7d1429f8a5fa6badaae44a1603ca8795787
Vallen Dsouza <vallendsouza@gmail.com>
2026-05-26 21:03:26 +0530

    11: widget screen router

---
+++ apps/widget/modules/widget/constants.ts
@@ -0,0 +1,12 @@
+export const WIDGET_SCREENS = [
+    "error",
+    "loading",
+    "selection",
+    "voice",
+    "auth",
+    "inbox",
+    "chat",
+    "contact",
+] as const;
+
+export const CONTACT_SESSION_KEY = "velora_ai_contact_session";
